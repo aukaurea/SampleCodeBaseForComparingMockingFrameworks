79b661ee692b96b6d18bbc00248a847d9ea8b0ed
Md. Alim Ul karim <alim.karim@aurea.com>
2019-02-06 21:52:14 +0600

    SwitchBlockadded
    SwitchBlockadded

---
+++ SampleCodeBase/MethodPropertiesWithBusinessValue/ExampleEnum1.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleCodeBase.MethodPropertiesWithBusinessValue
+{
+    public enum ExampleEnum1
+    {
+        Example1,
+        Example2,
+        Example3
+    }
+}
---
+++ SampleCodeBase/MethodPropertiesWithBusinessValue/SwitchCaseBlockExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCodeBase.MethodPropertiesWithBusinessValue
+{
+    public class SwitchCaseBlockExample
+    {
+        public SwitchCaseBlockExample()
+        {}
+
+        public void SwitchCaseExample1(ExampleEnum1 exampleEnum1)
+        {
+            switch (exampleEnum1)
+            {
+                case ExampleEnum1.Example1:
+                    Console.WriteLine("example1");
+                    Exmaple1();
+                    break;
+
+                case ExampleEnum1.Example2:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+
+            }
+        }
+
+        public void Exmaple1()
+        {
+            Console.WriteLine("Example1");
+        }
+    }
+}
