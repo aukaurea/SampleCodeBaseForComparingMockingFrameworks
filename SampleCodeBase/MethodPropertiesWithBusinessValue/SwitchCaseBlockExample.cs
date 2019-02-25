dba6f835d3df69ff01ae00fde5ca1ab92eb48faa
Md. Alim Ul karim <alim.karim@aurea.com>
2019-02-25 12:42:51 +0600

    switch case new blocks added
    switch case new blocks added

--- SampleCodeBase/MethodPropertiesWithBusinessValue/SwitchCaseBlockExample.cs
+++ SampleCodeBase/MethodPropertiesWithBusinessValue/SwitchCaseBlockExample.cs
@@ -11,7 +11,103 @@ namespace SampleCodeBase.MethodPropertiesWithBusinessValue
     public class SwitchCaseBlockExample
     {
         public SwitchCaseBlockExample()
-        {}
+        { }
+
+        public void SwitchCaseExample1(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("example1");
+                    Exmaple1();
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+
+            }
+        }
+
+        public void SwitchCaseExample1(short x)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("example1");
+                    Exmaple1();
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+
+            }
+        }
+
+        public void SwitchCaseExample1(long x)
+        {
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("example1");
+                    Exmaple1();
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+
+                case 3:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+
+            }
+        }
 
         public void SwitchCaseExample1(ExampleEnum1 exampleEnum1)
         {
@@ -66,6 +162,14 @@ namespace SampleCodeBase.MethodPropertiesWithBusinessValue
             var result = EnumHelper.GetRandomEnum<ExampleEnum1>();
 
             return result;
+        }
+
+        public void ForLoopExample1(int a, int b)
+        {
+            for (var i = 0; i < a && a < b; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
 
         public void Exmaple1()
