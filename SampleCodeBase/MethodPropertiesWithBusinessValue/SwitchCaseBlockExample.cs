d1a253aeba58d4bc48452b24c8d05564aef85612
Md. Alim Ul karim <alim.karim@aurea.com>
2019-03-04 16:09:24 +0600

    Added new switch case test examples
    Added new switch case test examples

--- SampleCodeBase/MethodPropertiesWithBusinessValue/SwitchCaseBlockExample.cs
+++ SampleCodeBase/MethodPropertiesWithBusinessValue/SwitchCaseBlockExample.cs
@@ -10,7 +10,7 @@ namespace SampleCodeBase.MethodPropertiesWithBusinessValue
 {
     public class SampleGetterValue{
 
-        public SampleGetterValue(IRequiredPrep required)
+        public SampleGetterValue(IRequiredPrep required, string w)
         {
             
         }
@@ -24,6 +24,14 @@ namespace SampleCodeBase.MethodPropertiesWithBusinessValue
         {
             return Guid.NewGuid().GetHashCode() + GetGetterInt();
         }
+    }
+
+    public static class ConstSwitch
+    {
+        public const string Sw1 = "Sw1";
+        public const string Sw2 = "Sw2";
+        public const string Sw3 = "Sw3";
+        public const string Sw4 = "Sw4";
     }
 
     public class SwitchCaseBlockExample
@@ -63,15 +71,84 @@ namespace SampleCodeBase.MethodPropertiesWithBusinessValue
             }
         }
 
+
+        public void SwitchCaseExampleStringEx1(string x)
+        {
+            switch (x)
+            {
+                case "Hello World":
+                    Console.WriteLine("example1");
+                    Exmaple1();
+
+                    break;
+
+                case "Hello World2":
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+
+                case "Hello World3":
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+            }
+        }
+
+        public void SwitchCaseExampleStringEx2(string x)
+        {
+            switch (x)
+            {
+                case ConstSwitch.Sw1:
+                    Console.WriteLine("example1");
+                    Exmaple1();
+
+                    break;
+
+                case ConstSwitch.Sw2:
+                    Console.WriteLine("example2");
+                    Exmaple1();
+
+                    break;
+
+
+                case ConstSwitch.Sw3:
+                    Console.WriteLine("example3");
+                    Exmaple1();
+
+                    break;
+
+                case ConstSwitch.Sw4:
+                    Console.WriteLine("example4");
+                    Exmaple1();
+
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    Exmaple1();
+
+                    break;
+            }
+        }
+
         public void SwitchCaseExample6(int? x)
         {
             var ir = new RequiredPrep();
-            var w1 = new SampleGetterValue(ir);
+            var xw = ir.SampleMethodString();
+            var w1 = new SampleGetterValue(ir, xw);
 
             var w = w1.GetGetterInt();
             var w3 = w1.GetGetterIntRandom2();
 
-            switch (w)
+            switch (w1.GetGetterInt())
             {
                 case (int) ExampleEnum1.Example1:
                     Console.WriteLine("example1");
