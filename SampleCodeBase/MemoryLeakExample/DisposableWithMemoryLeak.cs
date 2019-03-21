7668f1abd9617e03b751452c41ab7a0a4657ef31
Konstantinos Papakonstantinou <konstantinos.p@aurea.com>
2019-03-21 13:31:19 +0200

    Adding memory leak detection sample classes

---
+++ SampleCodeBase/MemoryLeakExample/DisposableWithMemoryLeak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace SampleCodeBase.MemoryLeakExample
+{
+    public class DisposableWithMemoryLeak : IDisposable
+    {
+        private readonly Timer _timer;
+
+        public DisposableWithMemoryLeak()
+        {
+            _timer = new Timer(Tick);
+            _timer.Change(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+        }
+
+        private static void Tick(object state)
+        {
+            Console.WriteLine("Tick");
+        }
+
+        public void Dispose()
+        {
+            // Timer is not disposed here, causing a memory leak
+        }
+    }
+}
---
+++ SampleCodeBase/MemoryLeakExample/DisposableWithoutMemoryLeak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace SampleCodeBase.MemoryLeakExample
+{
+    public class DisposableWithoutMemoryLeak : IDisposable
+    {
+        private readonly Timer _timer;
+
+        public DisposableWithoutMemoryLeak()
+        {
+            _timer = new Timer(Tick);
+            _timer.Change(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5));
+        }
+
+        private static void Tick(object state)
+        {
+            Console.WriteLine("Tick");
+        }
+
+        public void Dispose()
+        {
+            _timer.Dispose();
+        }
+    }
+}
