8864f7ab95396895b61de121d74d59882fac5301
Konstantinos Papakonstantinou <konstantinos.p@aurea.com>
2019-04-15 19:11:56 +0300

    Adding ClassUsingMethodOfInjectedService class

---
+++ SampleCodeBase/DependencyInjection/ClassUsingMethodOfInjectedService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace SampleCodeBase.DependencyInjection
+{
+    public class ClassUsingMethodOfInjectedService
+    {
+        private readonly IServiceInterface _service;
+
+        public ClassUsingMethodOfInjectedService(IServiceInterface service)
+        {
+            _service = service;
+        }
+=
+        public bool MethodCallingInjectedServiceMethodReturningBoolean()
+        {
+            if (_service.IsValid())
+            {
+                Console.WriteLine(1);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(2);
+                return false;
+            }
+        }
+
+        public void MethodCallingInjectedServiceMethodReturningIntUsedInEqualsCondition()
+        {
+            if (_service.GetCount() == 1)
+            {
+                Console.WriteLine(1);
+            }
+        }
+
+        public void MethodCallingInjectedServiceMethodReturningIntUsedInGreaterThanOrEqualsCondition()
+        {
+            if (_service.GetCount() >= 1)
+            {
+                Console.WriteLine(1);
+            }
+        }
+
+        public void MethodCallingInjectedServiceMethodReturningStringUsedInEqualsCondition()
+        {
+            if (_service.GetName() == "Maria")
+            {
+                Console.WriteLine(1);
+            }
+        }
+
+        public bool MethodCallingInjectedServiceMethodReturningReferenceType()
+        {
+            if (_service.GetFoo() != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //public void Do()
+        //{
+        //    _service.Do();
+
+        //    // Do stuff
+        //}
+    }
+}
